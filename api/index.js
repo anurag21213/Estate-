5f5f28eb48b0cbdfca7565f484294c0aa136a27f
Anurag Kumar <anurag.21213@knit.ac.in>
2023-12-28 18:57:46 +0530

    connected database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 const express=require('express')
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected succesfully")
+}).catch((err)=>{
+    console.log(err)
+})
 
 const app=express()
 
@@ -8,5 +17,5 @@ app.get('/',(req,res)=>{
 
 
 app.listen(3000,()=>{
-    console.log('Server is running.....')
+    console.log('Server is running......')
 })
